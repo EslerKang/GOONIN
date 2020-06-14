fcb0ec74edf050ca235264e686f6f7da4ac28933
강태준 <eslerkang@gmail.com>
2020-06-14 08:11:42 +0000

    jjamjji 페이지 어떻게 작성할지 고민중. 파일 입출력을 위한 함수만 생성

---
+++ js/jjamjji.js
@@ -0,0 +1,9 @@
+var today = new Date();
+var day = ["일","월","화","수","목","금","토"];
+document.write((today.getMonth()+1)+"<br>");
+document.write(today.getDate()+"<br>");
+document.write(day[today.getDay()]+"<br>");
+
+function openFile(){
+	
+}

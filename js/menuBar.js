10caa5ffc618ea0b4a06c949b9aea35256ea8023
EslerKang <31269150+EslerKang@users.noreply.github.com>
2020-06-14 14:53:45 +0900

    Add files via upload
    첫 업로드. 기본적인 홈페이지와 부가 페이지 포함

---
+++ js/menuBar.js
@@ -0,0 +1,16 @@
+document.write('\
+\
+\
+\
+\
+\
+<ul>\
+    <li><a href="home.html">HOME</a></li>\
+    <li><a href="jjamjji.html">탈출</a></li>\
+    <li><a href="succeed.html">인수인계</a></li>\
+    <li><a href="">C</a></li>\
+    <li><a href="">D</a></li>\
+</ul>\
+\
+\
+');

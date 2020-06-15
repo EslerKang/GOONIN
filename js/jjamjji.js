82c2a0f0a4f4f8258fe85b5fddf309b8cd2a33ff
- <undefined>
2020-06-15 14:11:04 +0000

    파일 입출력을 위한 함수 추가

--- js/jjamjji.js
+++ js/jjamjji.js
@@ -1,3 +1,6 @@
+var fileText = "";
+var fileContent;
+
 var today = new Date();
 var day = ["일","월","화","수","목","금","토"];
 document.write((today.getMonth()+1)+"<br>");
@@ -5,5 +8,25 @@ document.write(today.getDate()+"<br>");
 document.write(day[today.getDay()]+"<br>");
 
 function openFile(){
+	file = "../file/userList.txt";
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", file, false);
+	rawFile.onreadystatechange = function() {
+		if(rawFile.readyState === 4) {
+			if(rawFile.status === 200 || rawFile.status == 0) {
+				var text = rawFile.responseText;
+				fileText = text;
+				fileContent = fileText.split('\n');
+			}
+		}
+	};
+	rawFile.send(null);
+}
+
+
+function calcEscapeDate() {
 	
 }
+
+openFile();
+calcEscapeDate();
